8bd07cf7e85944018e40ad3a8fd448950bad7605
James Graca-Jones <jamesgracajones@gmail.com>
2023-01-28 19:49:47 +0000

    Enable clicking submit button to add a task to state if there is an input task

--- task-app/src/components/TaskLogger.js
+++ task-app/src/components/TaskLogger.js
@@ -5,14 +5,36 @@ class TaskLogger extends Component {
         super(props);
         this.taskInput = <input
             type='text'
+            className = 'TaskInput'
             placeholder='Type task name...'
         />
+        this.handleSubmission = this.handleSubmission.bind(this);
+    }
+    // TODO: consider making the input a child component and making its value 
+    // into state, this state being held by the TaskLogger
+    handleSubmission(event) {
+        const taskInput = this._getTaskInput(event);
+        const taskString = taskInput.value;
+        if (taskString) {
+            this.props.onTaskSubmission(taskInput.value);
+            taskInput.value = '';
+        }
+
+    }
+    _getTaskInput(event) {
+        const taskLoggerDiv = event.target.closest('.TaskLogger');
+        return taskLoggerDiv.querySelector('.TaskInput');
     }
     render() {
         return (
             <div className='TaskLogger'>
                 {this.taskInput}
-                <button type='button'>Add</button>
+                <button
+                    type='button'
+                    onClick={this.handleSubmission}
+                >
+                    Add
+                </button>
             </div>
         )
     }
