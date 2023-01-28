f9cafb5f24b425fef0f7ff4d9525a5d6b952f791
James Graca-Jones <jamesgracajones@gmail.com>
2023-01-28 19:20:30 +0000

    Render input part of task logger

--- task-app/src/components/TaskLogger.js
+++ task-app/src/components/TaskLogger.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 
 class TaskLogger extends Component {
+    constructor(props) {
+        super(props);
+        this.taskInput = <input
+            type='text'
+            placeholder='Type task name...'
+        />
+    }
     render() {
         return (
             <div className='TaskLogger'>
+                {this.taskInput}
             </div>
         )
     }
