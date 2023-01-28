1bdf74732bff3b219055f35f35bde6dc5a69f8d6
James Graca-Jones <jamesgracajones@gmail.com>
2023-01-28 17:39:03 +0000

    Make basic Overview class that renders unordered list for now

---
+++ task-app/src/components/Overview.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Overview extends Component {
+    render() {
+        return (
+            <ul className='Overview'>
+            </ul>
+        );
+    }
+}
+
+export default Overview
---
+++ task-app/src/components/TaskLogger.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class TaskLogger extends Component {
+    render() {
+        return (
+            <div className='TaskLogger'>
+            </div>
+        )
+    }
+}
+
+export default TaskLogger
