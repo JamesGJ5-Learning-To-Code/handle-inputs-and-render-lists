caed678d72d6b88343542cbd986fea6b65509ca8
James Graca-Jones <jamesgracajones@gmail.com>
2023-01-28 20:00:15 +0000

    Enable Overview to render the taskArray

--- task-app/src/components/Overview.js
+++ task-app/src/components/Overview.js
@@ -2,8 +2,14 @@ import React, { Component } from 'react';
 
 class Overview extends Component {
     render() {
+        const taskArray = this.props.taskArray;
         return (
             <ul className='Overview'>
+                {taskArray.map((taskString, index) =>
+                    <li key={index.toString()}>
+                        {taskString}
+                    </li>
+                )}
             </ul>
         );
     }
