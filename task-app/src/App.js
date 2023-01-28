2268b96a1f7054291dacc2081dee8dcbf1012700
James Graca-Jones <jamesgracajones@gmail.com>
2023-01-28 17:59:48 +0000

    Let App render a TaskLogger with App's addTask method in its props
    addTask adds a taskString to the taskArray, which is in App's state

--- task-app/src/App.js
+++ task-app/src/App.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
+import TaskLogger from './components/TaskLogger';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      taskArray: []
+    };
+    this.addTask = this.addTask.bind(this);
+  }
+  addTask(taskString) {
+    this.setState((currentState) => ({
+      taskArray: currentState.taskArray.concat(taskString)
+    }));
+  }
   render() {
     return (
       <div className="App">
+        <TaskLogger
+          onTaskSubmission={this.addTask}
+        />
       </div>
     );
   }
